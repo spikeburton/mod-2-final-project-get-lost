82b335fb43a1b9150c27f308e1b31e9c3863a31c
spikeburton <ralphburton3@gmail.com>
2019-03-05 13:15:06 -0500

    validate new user data

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   has_many :adventures, through: :user_adventures
   has_secure_password
   validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: /@/ }
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
 end
