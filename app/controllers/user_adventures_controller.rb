eeb4c99a4e80e8fe1c1dc8dc791f0bda21ea6ba4
spikeburton <ralphburton3@gmail.com>
2019-03-07 13:36:51 -0500

    change handling of showing an adventure a user has already saved

--- app/controllers/user_adventures_controller.rb
+++ app/controllers/user_adventures_controller.rb
@@ -1,16 +1,20 @@
 class UserAdventuresController < ApplicationController
-    def create
-        @adventure = Adventure.find(params[:adventure_id])
-        if current_user.add_adventure(@adventure)
-            redirect_to current_user
-        else
-            flash[:alert] = "You have already saved this Adventure."
-            render :'adventures/show'
-        end
+  def create
+    @adventure = Adventure.find(params[:adventure_id])
+    if current_user.add_adventure(@adventure)
+      redirect_to current_user
+    else
+      flash[:alert] = "An error occured."
+      render :'adventures/show'
     end
+  end
 
-    def destroy
-        current_user.remove_adventure(params[:id])
-        redirect_to current_user
+  def destroy
+    if current_user.remove_adventure(params[:id])
+      redirect_to current_user
+    else
+      flash[:alert] = "An error occured."
+      render :'users/show'
     end
+  end
 end
