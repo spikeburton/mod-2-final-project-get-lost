f2e1ddf0c73ba58c041f4c6abb6d825786832248
Chanda Grayson <chandagrayson@gmail.com>
2019-03-07 11:45:45 -0500

    working on user adding an adventure

--- app/controllers/user_adventures_controller.rb
+++ app/controllers/user_adventures_controller.rb
@@ -1,5 +1,11 @@
 class UserAdventuresController < ApplicationController
 
+    def create 
+        binding.pry
+        current_user.add_adventure(params[:id])
+        redirect_to current_user
+    end
+
     def destroy
         current_user.remove_adventure(params[:id])
         redirect_to current_user 
--- config/routes.rb
+++ config/routes.rb
@@ -6,14 +6,13 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/signup' => 'users#validate_info', as: 'validate'
   get '/settings' => 'users#edit'
-  post '/adventures/:id' => 'adventures#add_adventure_to_user', as: :add_adventure
   post '/adventures' => 'adventures#index'
 
   resources :adventures
   resources :users, except: [:new, :edit]
   # resources :user_adventures, only: [:destroy]
   resource :adventures do 
-    resources :user_adventures, only: [:destroy]
+    resources :user_adventures, only: [:create, :destroy]
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
